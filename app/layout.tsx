6b6a2ece71d57106e6c498b34606cb2bab7bb640
Reid Barber <reid@reidbarber.com>
2024-03-31 19:26:44 -0500

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           className: "class",
         }}
       />
+      <Analytics />
     </html>
   );
 }
