cdb002ed1952e84da04f61aedc91685a73286f46
Reid Barber <reid@reidbarber.com>
2025-07-23 00:27:56 -0500

    remove cusor-pointer from settings switch

--- components/SettingsSwitch.tsx
+++ components/SettingsSwitch.tsx
@@ -19,7 +19,7 @@ export function SettingsSwitch({
       style={{ WebkitTapHighlightColor: "transparent" }}
     >
       {label}
-      <span className="group-data-[selected]:bg-green-500 group-data-[focus-visible]:ring-2 h-6 w-9 cursor-pointer rounded-full border-2 border-transparent bg-slate-400 ring-offset-2 ring-offset-white dark:ring-offset-black transition duration-200">
+      <span className="group-data-[selected]:bg-green-500 group-data-[focus-visible]:ring-2 h-6 w-9 rounded-full border-2 border-transparent bg-slate-400 ring-offset-2 ring-offset-white dark:ring-offset-black transition duration-200">
         <span className="group-data-[selected]:ml-3 group-data-[selected]:group-data-[pressed]:ml-2 group-data-[pressed]:w-6 block h-5 w-5 origin-right rounded-full bg-white shadow transition-all duration-200" />
       </span>
     </Switch>
